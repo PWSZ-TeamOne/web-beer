494cc711e728514d646703170e85c6648e930468
Karol Zygadło <karol.zygadlo9713@gmail.com>
2020-11-15 19:09:50 +0100

    Update main.js
    hide config

--- src/main.js
+++ src/main.js
@@ -10,14 +10,14 @@ import { sync } from 'vuex-router-sync'
 import VueFlashMessage from 'vue-flash-message'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_leXc0RXQiANmSNPUHj5x6Snk-RCYIg4",
-  authDomain: "web-beer-8373c.firebaseapp.com",
-  databaseURL: "https://web-beer-8373c.firebaseio.com",
-  projectId: "web-beer-8373c",
-  storageBucket: "web-beer-8373c.appspot.com",
-  messagingSenderId: "453391520452",
-  appId: "1:453391520452:web:a6871718bee92faa4faf91",
-  measurementId: "G-ZPQJR1BY67"
+  apiKey: "###",
+  authDomain: "###",
+  databaseURL: "###",
+  projectId: "###",
+  storageBucket: "###",
+  messagingSenderId: "###",
+  appId: "###",
+  measurementId: "###"
 };
 
 firebase.initializeApp(firebaseConfig);
